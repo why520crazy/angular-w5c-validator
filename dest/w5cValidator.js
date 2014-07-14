37412b051cfbf87948587329416a3c322747dff8
why520crazy <why520crazy@163.com>
2014-07-14 15:17:00 +0800

    build w5cValidator.js

--- dest/w5cValidator.js
+++ dest/w5cValidator.js
@@ -5,15 +5,15 @@
  * License: NONE
  */
 angular.module("w5c.validator", ["ng"])
-    .provider('w5cValidator', function () {
+    .provider('w5cValidator', [function () {
         var defaultRules = {
-                required      : "该选项不能为空",
-                maxlength     : "该选项输入值长度不能大于{maxlength}",
-                minlength     : "该选项输入值长度不能小于{minlength}",
-                email         : "输入邮件的格式不正确",
-                repeat        : "两次输入不一致",
-                pattern       : "该选项输入格式不正确",
-                number        : "必须输入数字",
+                required: "该选项不能为空",
+                maxlength: "该选项输入值长度不能大于{maxlength}",
+                minlength: "该选项输入值长度不能小于{minlength}",
+                email: "输入邮件的格式不正确",
+                repeat: "两次输入不一致",
+                pattern: "该选项输入格式不正确",
+                number: "必须输入数字",
                 w5cuniquecheck: "该输入值已经存在，请重新输入"
             },
             elemTypes = ['text', 'password', 'email', 'number', ['textarea'], ['select'], ['select-one']];
@@ -52,14 +52,14 @@ angular.module("w5c.validator", ["ng"])
         };
 
         validatorFn.prototype = {
-            constructor     : validatorFn,
-            config          : function (options) {
+            constructor: validatorFn,
+            config: function (options) {
                 this.options = angular.extend(this.options, options);
             },
-            setRules        : function (rules) {
+            setRules: function (rules) {
                 this.rules = rules;
             },
-            getErrorMessage : function (validationName, elem) {
+            getErrorMessage: function (validationName, elem) {
                 var msgTpl = null;
                 if (!this.isEmpty(this.rules[elem.name]) && !this.isEmpty(this.rules[elem.name][validationName])) {
                     msgTpl = this.rules[elem.name][validationName];
@@ -98,7 +98,7 @@ angular.module("w5c.validator", ["ng"])
                 }
                 return elementErrors;
             },
-            showError       : function (elem, errorMessages, options) {
+            showError: function (elem, errorMessages, options) {
                 var useOptions = angular.extend({}, this.options, options);
                 if (useOptions.showError === false) {
                     return;
@@ -110,7 +110,7 @@ angular.module("w5c.validator", ["ng"])
                     return this.defaultShowError(elem, errorMessages);
                 }
             },
-            removeError     : function (elem, options) {
+            removeError: function (elem, options) {
                 var useOptions = angular.extend({}, this.options, options);
                 if (useOptions.removeError === false) {
                     return;
@@ -137,7 +137,7 @@ angular.module("w5c.validator", ["ng"])
         this.$get = function () {
             return validator;
         }
-    });
+    }]);
 angular.module("w5c.validator")
     .directive("w5cFormValidate", ['$parse', 'w5cValidator', function ($parse, w5cValidator) {
         return{
