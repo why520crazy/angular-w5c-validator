ee4106db1a2477e1c24cdc98d504161f32588c6b
why520crazy <why520crazy@163.com>
2014-09-04 14:10:33 +0800

    w5c-repeat directive的demo示例

--- example/index.js
+++ example/index.js
@@ -8,26 +8,29 @@
 
         // 全局配置
         w5cValidatorProvider.config({
-            blurTrig   : false,
-            showError  : true,
+            blurTrig: false,
+            showError: true,
             removeError: true
 
         });
         w5cValidatorProvider.setRules({
-            email   : {
+            email: {
                 required: "输入的邮箱地址不能为空",
-                email   : "输入邮箱地址格式不正确"
+                email: "输入邮箱地址格式不正确"
             },
             username: {
                 required: "输入的用户名不能为空",
-                pattern : "用户名必须输入字母、数字、下划线,以字母开头"
+                pattern: "用户名必须输入字母、数字、下划线,以字母开头"
             },
             password: {
-                required : "密码不能为空",
+                required: "密码不能为空",
                 minlength: "密码长度不能小于{minlength}",
                 maxlength: "密码长度不能大于{maxlength}"
             },
-            number  : {
+            repeatPassword: {
+                repeat: "两次密码输入不一致"
+            },
+            number: {
                 required: "数字不能为空"
             }
         });
@@ -35,7 +38,7 @@
     app.controller("validateCtrl", ["$scope", "$http", function ($scope, $http) {
 
         var vm = $scope.vm = {
-            htmlSource   : "",
+            htmlSource: "",
             showErrorType: 1
         };
 
@@ -53,7 +56,7 @@
             if (vm.showErrorType == 2) {
                 vm.validateOptions.showError = false;
                 vm.validateOptions.removeError = false;
-            }else{
+            } else {
                 vm.validateOptions.showError = true;
                 vm.validateOptions.removeError = true;
             }
